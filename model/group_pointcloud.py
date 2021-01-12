d49ece025bea75c7e02c21bd3d9d6dc894a17199
Bhoopen <62685221+kasai2210@users.noreply.github.com>
2021-01-12 22:55:52 +0530

    Update group_pointcloud.py

--- model/group_pointcloud.py
+++ model/group_pointcloud.py
@@ -25,7 +25,7 @@ class VFELayer(object):
         pointwise = self.batch_norm.apply(self.dense.apply(inputs), training)
 
         #n [K, 1, units]
-        aggregated = tf.reduce_max(pointwise, axis=1, keep_dims=True)
+        aggregated = tf.reduce_max(pointwise, axis=1, keepdims=True)
 
         # [K, T, units]
         repeated = tf.tile(aggregated, [1, cfg.VOXEL_POINT_COUNT, 1])
